84b7cfedc4a75e0855a59296c97e1f5f2878aa2e
Franco Valdes <franco.valdes89@gmail.com>
2020-03-16 12:48:40 -0400

    update meta... still need an open graph image

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -107,8 +107,8 @@ const Header: React.FC<HeaderProps> = ({
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
-        <meta property="og:site_name" content="Petly" />
-        <meta property="og:url" content="https://getpetly.com" />
+        <meta property="og:site_name" content="Teqnical" />
+        <meta property="og:url" content="https://teqnical.now.sh" />
         <meta property="og:description" content={description} />
         <meta property="og:type" content="website" />
         <meta property="og:image" content={image} />
